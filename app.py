d73a5c4f7c084a09c1c60c568c4644a6e4d9ce9a
Tiago Lobato da Gama <tiago.gama@grad.ufsc.br>
2021-07-20 13:19:57 -0300

    mudança e adição bots

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda"),BotFeliz('Felizciano'),BotTriste('nietzsche Pereira')]
+lista_bots = [BotZangado("Zangiano da Silva"),BotFeliz('Felizciano'),BotTriste('nietzsche Pereira')]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
